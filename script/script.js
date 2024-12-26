a169e428adbe0866692eaaebb0ce335d1d9aa625
soulxmark <mark2899estimada@gmail.com>
2024-12-27 07:46:30 +0800

    hamburger navigation

--- script/script.js
+++ script/script.js
@@ -11,6 +11,14 @@ function checkInView() {
     }
   });
 }
+const hamburger = document.getElementById('hamburger');
+        const navLinks = document.getElementById('navLinks');
+        const body = document.body;
 
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+            hamburger.classList.toggle('open');
+            body.classList.toggle('menu-active');  // Add or remove background dimming
+        });
 window.addEventListener('scroll', checkInView);
 window.addEventListener('load', checkInView); // For when the page loads
