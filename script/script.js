73ce72f22206c6ce4fa9231a90352c511af7b649
soulxmark <mark2899estimada@gmail.com>
2025-01-17 00:12:20 +0800

    changes

--- script/script.js
+++ script/script.js
@@ -19,6 +19,6 @@ const hamburger = document.getElementById('hamburger');
             navLinks.classList.toggle('active');
             hamburger.classList.toggle('open');
             body.classList.toggle('menu-active');  // Add or remove background dimming
-        });
+        })
 window.addEventListener('scroll', checkInView);
 window.addEventListener('load', checkInView); // For when the page loads
